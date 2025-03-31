f7dd7ecde028a107efb5d8b069464addd4f0d8d4
akirata32 <tr3nzinho@gmail.com>
2025-03-31 08:39:19 -0300

    first commit

---
+++ ex01.py
@@ -0,0 +1,4 @@
+nome= input("Qual o seu nome?")
+idade= input("qual sua idade")
+salario=input("diga o salario hehehehe")
+print(f"seu nome,idade e salario são respectivamente {nome}, {idade}, {salario}")

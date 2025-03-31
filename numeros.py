d64312c77c7478ab712de584973b7305bb12541d
akirata32 <tr3nzinho@gmail.com>
2025-03-31 09:43:24 -0300

    operadores

---
+++ numeros.py
@@ -0,0 +1,11 @@
+n1=int (input("digite um n"))
+n2=int (input("digite o segundo n"))
+soma= n1+n2
+sub= n1-n2
+mult= n1*n2
+div= n1/n2
+
+print(f"{n1}+{n2} o resultado é{soma}")
+print(f"{n1}-{n2} o resultado é{sub}")
+print(f"{n1}*{n2} o resultado é{mult}")
+print(f"{n1}/{n2} o resultado é{div}")

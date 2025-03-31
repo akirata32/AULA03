b8c35e5e23115138d1d3b108b8790a35357ba8e7
akirata32 <tr3nzinho@gmail.com>
2025-03-31 11:08:12 -0300

    media simples

---
+++ ex05.py
@@ -0,0 +1,6 @@
+n1=int(input("diga sua n1"))
+n2=int(input("diga sua nota 2"))
+n3=int(input("diga sua nota 3"))
+media= (n1+n2+n3)/3
+if (media>=7) :print("PASSOU DE ANO")
+else :print("RECUPERAÇÃO HAHAHAHAH")
